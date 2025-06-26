58caca0a9a951eed8d72f434a43d38ce03408f50
thele <gradf@leond.co.uk>
2025-06-26 14:30:48 +0100

    Improved precision. All resampling is performed using 32 bits per channel rather than 8. We then compute 8 bit channels from the final image from them.
    In addition to that, we now map to linear color space, perform computations and apply the inverse mapping back to srb color space. This improves color accuracy.

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@
 #pragma warning(pop)
 
 struct Pixel {
-	uint8_t r{};
-	uint8_t g{};
-	uint8_t b{};
+	float r{};
+	float g{};
+	float b{};
 	
 	Pixel& operator+=(const Pixel& rhs) {
 		this->r += rhs.r;
@@ -20,16 +20,16 @@ struct Pixel {
 	}
 
 	Pixel& operator/=(const float rhs) {
-		this->r = static_cast<uint8_t>(this->r / rhs);
-		this->g = static_cast<uint8_t>(this->g / rhs);
-		this->b = static_cast<uint8_t>(this->b / rhs);
+		this->r = this->r / rhs;
+		this->g = this->g / rhs;
+		this->b = this->b / rhs;
 		return *this;
 	}
 
 };
 
 Pixel operator*(const Pixel& lhs, const float rhs) {
-	return Pixel{ static_cast<uint8_t>(lhs.r * rhs), static_cast<uint8_t>(lhs.g * rhs), static_cast<uint8_t>(lhs.b * rhs) };
+	return Pixel{ lhs.r * rhs, lhs.g * rhs, lhs.b * rhs };
 }
 
 struct Extent2D {
@@ -48,9 +48,6 @@ int compute_radius(Extent2D in_image_size, Extent2D out_image_size) {
 
 float mitchell_netravali(float x, int radiusFactor) {
 
-	//negative lobes
-	float radius_factor{ radiusFactor + 0.1f };
-
 	float abs_x{ std::fabs(x / radiusFactor) };
 
 	float B = 1.0f / 3.0f;
@@ -58,14 +55,21 @@ float mitchell_netravali(float x, int radiusFactor) {
 
 	if (abs_x <= 1) {
 		return ((12 - 9 * B - 6 * C) * abs_x * abs_x * abs_x +
-			(-18 + 12 * B + 6 * C) * abs_x * abs_x + (6 - 2 * B)) /	6 / radius_factor;
+			(-18 + 12 * B + 6 * C) * abs_x * abs_x + (6 - 2 * B)) /	6 / radiusFactor;
 	}
 	else if ((1 <= abs_x) && (abs_x <= 2)) {
 		return ((-B - 6 * C) * abs_x * abs_x * abs_x + (6 * B + 30 * C) * abs_x * abs_x +
-			(-12 * B - 48 * C) * abs_x + (8 * B + 24 * C)) / 6 / radius_factor;
+			(-12 * B - 48 * C) * abs_x + (8 * B + 24 * C)) / 6 / radiusFactor;
 	}
 
 	return 0.0f;
+}
+
+float toSRGB(float linear) {
+	if (linear < 0.0031308f)
+		return linear * 12.92f;
+
+	return 1.055f * pow(linear, 0.41666f) - 0.055f;
 }
 
 /*		image is 8 bits per channel, 24 bits per pixel		*/
@@ -75,13 +79,16 @@ int main()
 	// read image data in
 	int x{}, y{};
 	// load image and reinterpret cast to Pixel* to improve code readability (avoid pointer arithmetic with magic numbers)
-	Pixel* in_image{ reinterpret_cast<Pixel*>(stbi_load("images/red_panda.jpg", &x, &y, nullptr, STBI_rgb)) };
+	// also performs the conversion from srgb to linear for us, we'll have to perform the inverse on the way out
+	Pixel* in_image{ reinterpret_cast<Pixel*>(stbi_loadf("images/red_panda.jpg", &x, &y, nullptr, STBI_rgb)) };
 
 	// check if image load failed
 	if (!in_image) {
 		std::cout << stbi_failure_reason();
 		return EXIT_FAILURE;
 	}
+
+	//nonlinear to linear conversion
 
 	// set input and output image extent
 	float scale_factor{ 1.0f/3.0f };
@@ -91,11 +98,12 @@ int main()
 	// compute filter radius
 	int filter_radius{ compute_radius(in_image_extent, out_image_extent) };
 
-	Pixel* out_image{ reinterpret_cast<Pixel*>(malloc(out_image_extent.width * out_image_extent.height * sizeof(Pixel))) };
+	Pixel* intermediate_image{ reinterpret_cast<Pixel*>(malloc(out_image_extent.width * out_image_extent.height * sizeof(Pixel))) };
 	Pixel* S{ reinterpret_cast<Pixel*>(malloc(out_image_extent.width * in_image_extent.height * sizeof(Pixel))) };
+	uint8_t* out_image{ reinterpret_cast<uint8_t*>(malloc(static_cast<std::size_t>(out_image_extent.width * out_image_extent.height * 3))) };
 
-	if (out_image == nullptr || S == nullptr) {
-		std::cout << "Failed to allocate resample buffer" << '\n';
+	if (intermediate_image == nullptr || S == nullptr || out_image == nullptr) {
+		std::cout << "Failed to allocate resample buffers." << '\n';
 		return EXIT_FAILURE;
 	}
 
@@ -118,7 +126,7 @@ int main()
 			// relative position in input image
 			float _x{ j * delta_x + delta_x / 2.0f };
 			int _x_ceil{ static_cast<int>(std::ceil(_x)) };
-			S[i * out_image_extent.width + j] = Pixel{ 0,0,0 };
+			S[i * out_image_extent.width + j] = Pixel{ 0.0f,0.0f,0.0f };
 			float normalize_factor{ 0 };
 
 			for (int j_prime{ _x_ceil - filter_radius }; j_prime <= _x + filter_radius; ++j_prime) {
@@ -139,25 +147,35 @@ int main()
 		int _y_ceil{ static_cast<int>(std::ceil(_y)) };
 		for (int j{ 0 }; j < out_image_extent.width; ++j) {
 
-			// relative position in input image
- 			out_image[i * out_image_extent.width + j] = Pixel{ 0,0,0 };
+			// relative position in input image`	`	
+			intermediate_image[i * out_image_extent.width + j] = Pixel{ 0,0,0 };
 			float normalize_factor{ 0 };
 
 			for (int i_prime{ _y_ceil - filter_radius }; i_prime <= _y + filter_radius; ++i_prime) {
 				if (i_prime >= 0 && i_prime < in_image_extent.height) {
 					float weight{ mitchell_netravali(_y - i_prime, filter_radius) };
-					out_image[i * out_image_extent.width + j] += S[i_prime * out_image_extent.width + j] * weight;
+					intermediate_image[i * out_image_extent.width + j] += S[i_prime * out_image_extent.width + j] * weight;
 					normalize_factor += weight;
 				}
 			}
-			out_image[i * out_image_extent.width + j] /= normalize_factor;
+			intermediate_image[i * out_image_extent.width + j] /= normalize_factor;
 		}
+	}
+
+	// linear -> srgb
+#pragma omp parallel for
+	for (int i { 0 }; i < out_image_extent.width * out_image_extent.height; ++i) {
+		out_image[i*3] = static_cast<uint8_t>(std::floor(toSRGB(intermediate_image[i].r) * 255));
+		out_image[i*3+1] = static_cast<uint8_t>(std::floor(toSRGB(intermediate_image[i].g) * 255));
+		out_image[i*3+2] = static_cast<uint8_t>(std::floor(toSRGB(intermediate_image[i].b) * 255));
 	}
 
 	stbi_write_jpg("red_panda_cubic.jpg", static_cast<int>(out_image_extent.width), static_cast<int>(out_image_extent.height), 3, reinterpret_cast<void*>(out_image), 100);
 	stbi_image_free(reinterpret_cast<void*>(in_image));
+	free(reinterpret_cast<void*>(intermediate_image));
 	free(reinterpret_cast<void*>(out_image));
 	free(reinterpret_cast<void*>(S));
 
 	return 0;
 }
+
